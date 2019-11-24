955b4f4ed57dfb1c76ce91e8f1db1a646df1ac3e
Shaikat <49788830+shaikat17@users.noreply.github.com>
2019-11-25 00:36:52 +0600

    Create Between Two Sets.cpp

---
+++ Between Two Sets.cpp
@@ -0,0 +1,145 @@
+Problem Link: https://www.hackerrank.com/challenges/between-two-sets/problem
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'getTotalX' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER_ARRAY a
+ *  2. INTEGER_ARRAY b
+
+ */
+
+int lcd(vector<int> a)
+{
+    int ans=a[0];
+    for(int i=0; i<a.size(); i++)
+{
+
+    ans =(ans*a[i])/__gcd(ans, a[i]);
+}
+return ans;
+}
+
+int gcd(vector<int> a)
+{
+    int ans = a[0];
+    for(int i=0; i<a.size(); i++)
+{
+
+    ans = __gcd(ans, a[i]);
+}
+return ans;
+}
+
+int getTotalX(vector<int> a, vector<int> b) {
+
+int x, y, cnt=0;
+
+x=lcd(a);
+y=gcd(b);
+
+for(int i=x, j=2; i<=y; i=x*j, j++)
+{ 
+    if(y%i==0) cnt++;
+
+    
+}
+return cnt;
+
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string first_multiple_input_temp;
+    getline(cin, first_multiple_input_temp);
+
+    vector<string> first_multiple_input = split(rtrim(first_multiple_input_temp));
+
+    int n = stoi(first_multiple_input[0]);
+
+    int m = stoi(first_multiple_input[1]);
+
+    string arr_temp_temp;
+    getline(cin, arr_temp_temp);
+
+    vector<string> arr_temp = split(rtrim(arr_temp_temp));
+
+    vector<int> arr(n);
+
+    for (int i = 0; i < n; i++) {
+        int arr_item = stoi(arr_temp[i]);
+
+        arr[i] = arr_item;
+    }
+
+    string brr_temp_temp;
+    getline(cin, brr_temp_temp);
+
+    vector<string> brr_temp = split(rtrim(brr_temp_temp));
+
+    vector<int> brr(m);
+
+    for (int i = 0; i < m; i++) {
+        int brr_item = stoi(brr_temp[i]);
+
+        brr[i] = brr_item;
+    }
+
+    int total = getTotalX(arr, brr);
+
+    fout << total << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
